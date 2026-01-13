d2f72f5189ad58f221d8b4664c033f4a376654ea
Vinicius Saraiva <vinicius@s4r41va.com>
2026-01-13 00:07:05 -0300

    Configuração final Docker e animações

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// src/app/layout.tsx
 import type { Metadata } from "next";
-import { Inter } from "next/font/google"; // Vamos usar uma fonte clean
+import { Inter } from "next/font/google"; 
 import "./globals.css";
-import { ReactLenis } from "@/utils/lenis"; // Vamos criar esse utilitário já já
+import { ReactLenis } from "@/utils/lenis"; 
 
 const inter = Inter({ subsets: ["latin"], weight: ["300", "400", "600"] });
 
@@ -18,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${inter.className} bg-black text-white antialiased`}>
-        {/* Envolvemos tudo no Smooth Scroll */}
+        {/* AQUI ESTÁ A CORREÇÃO: */}
         <ReactLenis root>
-          {children}
+          
+          {children as any}
         </ReactLenis>
       </body>
     </html>
